3c33c51f3759291124aa37de7d3002af4dce14e0
Arin Baral <arinbaral20@gmail.com>
2026-06-15 09:36:35 +0545

    Add web version of voting system - HTML, CSS, JS, and data files

---
+++ data.js
@@ -0,0 +1,75 @@
+const electionData = {
+    constituencies: [
+        {
+            id: 1,
+            name: "Jhapa-5",
+            candidates: [
+                { name: "KP Sharma Oli", party: "UML" },
+                { name: "Balen Shah", party: "Independent" },
+                { name: "Keshav Prasad Budhathoki", party: "Congress" },
+                { name: "Rajendra Lingden", party: "RPP" }
+            ]
+        },
+        {
+            id: 2,
+            name: "Kathmandu-1",
+            candidates: [
+                { name: "Ranju Darshana Neupane", party: "Bibeksheel" },
+                { name: "Mohan Raj Regmi", party: "RSP" },
+                { name: "Prabal Thapa", party: "Congress" },
+                { name: "Rabindra Mishra", party: "RPP" }
+            ]
+        },
+        {
+            id: 3,
+            name: "Chitwan-2",
+            candidates: [
+                { name: "Rabi Lamichhane", party: "RSP" },
+                { name: "Surya Prasad Adhikari", party: "UML" },
+                { name: "Umesh Shrestha", party: "Congress" },
+                { name: "Vikram Pandey", party: "RPP" }
+            ]
+        },
+        {
+            id: 4,
+            name: "Pokhara-3",
+            candidates: [
+                { name: "Dhan Raj Gurung", party: "Congress" },
+                { name: "Krishna Thapa", party: "UML" },
+                { name: "Guru Raj Baral", party: "UML" },
+                { name: "Deepak Bahadur Singh", party: "Congress" }
+            ]
+        },
+        {
+            id: 5,
+            name: "Biratnagar-4",
+            candidates: [
+                { name: "Nagarik Shah", party: "Independent" },
+                { name: "Lal Babu Pandit", party: "UML" },
+                { name: "Amrit Aryal", party: "Congress" },
+                { name: "Rajendra Lingden", party: "RPP" }
+            ]
+        },
+        {
+            id: 6,
+            name: "Sarlahi-4",
+            candidates: [
+                { name: "Amresh Kumar Singh", party: "Congress" },
+                { name: "Amanish Kumar Yadav", party: "Janamat" },
+                { name: "Gagan Kumar Thapa", party: "Congress" },
+                { name: "Pradeep Kumar Jha", party: "Loktantrik" }
+            ]
+        },
+        {
+            id: 7,
+            name: "Bhaktapur-2",
+            candidates: [
+                { name: "Rajiv Khatri", party: "Congress" },
+                { name: "Mahesh Basnet", party: "UML" },
+                { name: "Kabir Rana", party: "Independent" },
+                { name: "Bikram Thapa", party: "RSP" }
+            ]
+        }
+    ],
+    adminPassword: "admin123"
+};
